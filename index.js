c3a7b3b47150f45ca3d2416ddee3afdb9dc27c91
Hassan Mohsen <149179822+hassanmohsen1@users.noreply.github.com>
2025-04-14 10:57:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get('/', (req,res) =>{
 });
 
 app.get('/index', (req,response) =>{
+    const message = `Hello World, I am pod :version 1.2.0` ; 
     let url = "https://en.wikipedia.org/w/api.php"
     let params = {
         action: "opensearch",
